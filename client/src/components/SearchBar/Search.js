27f9850a124acbe9e239529bcc8380ad5ea21b38
santiago ferrera <saferrera.sf@gmail.com>
2021-06-04 02:14:02 -0300

    pokedex and searchbar

--- client/src/components/SearchBar/Search.js
+++ client/src/components/SearchBar/Search.js
@@ -1,34 +1,68 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React,{useState} from "react";
+import { useSelector,useDispatch } from "react-redux";
+import { getPokemon } from "../../redux/actions/actions";
 import "./Search.css";
 
 function PokemonBuscado() {
+  const dispatch = useDispatch();
   const pokemons = useSelector((store) => store.pokemons);
 
+const[input,setInput]= useState("");
+
+function handleChange(event) {
+  setInput(event.target.value);
+};
+function handleSubmit(event) {
+  event.preventDefault();
+  dispatch(getPokemon(input))
+}
   return (
     <>
-      <div className="pokedex-container">
-        <div className="dot-container">
-          <div className="dot1"></div>
-          <div className="dot2"></div>
-          <div className="dot3"></div>
-        </div>
+      <div className="father">
+        <div className="pokedex-container">
+          <div className="dot-container">
+            <div className="dot1"></div>
+            <div className="dot2"></div>
+            <div className="dot3"></div>
+          </div>
 
-        <div className="img-container">
-          <img src={pokemons?.preview} alt="img not found" />
+          <div className="img-container">
+            <img src={pokemons?.preview} alt="img not found" />
 
-          <div className="types-container">
-            {pokemons?.types?.map((t) => (
-              <p>{t.name}</p>
-            ))}
+            <div className="types-container">
+              {pokemons?.types?.map((t) => (
+                <p>{t.name}</p>
+              ))}
+            </div>
+          </div>
+
+          <div className="stats-container">
+            <p> Name: {pokemons.name}</p>
           </div>
         </div>
-
-        <div className="stats-container">
-           <p> Name: {pokemons.name}</p>
+        <div className="bizagra">
+          <div className="dotbiz1"></div>
+          <div className="dotbiz2"></div>
+          <div className="dotbiz3"></div>
+          <div className="dotbiz4"></div>
+        </div>
+        <div className="searchbar-container">
+          <form className="searchbar" onSubmit={(e) =>handleSubmit(e)}>
+          <input
+          type="text"
+          id="name"
+          autoComplete="off"
+          value={input}
+          onChange={(e) =>handleChange(e)}
+        />
+        <div className= "search-button">
+          <button type="submit">SEARCH</button>
+        </div>
+          </form>
+          <div className="green-pad"></div>
+          <div className="select-button"></div>
         </div>
       </div>
-    
     </>
   );
 }
